ed86863cd3a7a466557fca2f85f4c48304cdbc0a
saran <selvakumarraj432@gmail.com>
2025-06-13 16:57:21 +0530

    vercel file added

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
 app.use(cors({
   origin: ['http://localhost:3000', 'https://create-api-liard.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -25,10 +24,6 @@ app.use(cors({
 // Routes
 app.use('/api/products', productRoutes);
 
-// Root route
-app.get('/', (req, res) => {
-  res.send('✅ Product API Running');
-});
 
 // Global Error Handler
 app.use((err, req, res, next) => {
